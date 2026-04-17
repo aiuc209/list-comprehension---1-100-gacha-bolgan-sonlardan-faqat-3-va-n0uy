5c07c2367f9d26650ff35c7e0624d61d1335e71d
aiuc209 <dzuiryuomdbaoy@gmail.com>
2026-04-17 22:29:00 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,11 @@
+"""
+List Comprehension   1 100 Gacha Bolgan Sonlardan Faqat 3 Va N0uy
+Topic: LIST COMPREHENSION - 1: ��100 gacha bo‘lgan sonlardan faqat 3 va 5 ga bo‘linadiganlarini tanlang. Yangi ro‘yxat hosil qiling: [15, 30, 45, ... 90]
+"""
+
+def main():
+    print("Welcome to List Comprehension   1 100 Gacha Bolgan Sonlardan Faqat 3 Va N0uy!")
+    print("Muvaffaqiyatli ishga tushirildi.")
+
+if __name__ == "__main__":
+    main()
